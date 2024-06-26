e085b6adc08753b0cf412d22be06405135d0159b
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-06-26 09:24:09 +0530

    Update database.py

--- database.py
+++ database.py
@@ -4,5 +4,4 @@ import os
 # Connect to MongoDB
 mongo_client = MongoClient(os.getenv('mongodb'))
 database = mongo_client['Movie']
-collection = os.getenv('movie_info')
-movie_info = database[collection]
+movie_info = database['movie_info']
