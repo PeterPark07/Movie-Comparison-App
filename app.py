11d6b4be9fb4435864ac8bf8cc32013426a27c93
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-06-26 11:39:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # Fetch movies from MongoDB sorted by votes in descending order
+    movies = list(movie_collection.find().sort([('votes', -1)]))
+    return render_template('index.html', movies=movies)
 
 @app.route('/compare')
 def compare():
@@ -18,7 +20,7 @@ def compare():
 def vote():
     winner_id = request.form['winner_id']
     winner_title = request.form['winner_title']
-    
+
     # Update vote count for the winner in MongoDB
     # Try to find the movie by ID, if not found, create a new document
     movie = movie_collection.find_one({'id': winner_id})
@@ -26,7 +28,7 @@ def vote():
         movie_collection.update_one({'id': winner_id}, {'$inc': {'votes': 1}})
     else:
         movie_collection.insert_one({'id': winner_id, 'title': winner_title, 'votes': 1})
-    
+
     return redirect(url_for('compare'))
 
 @app.route('/skip', methods=['POST'])
