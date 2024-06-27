2c84b0d862997635fcd1ba3ceedc0cc87f4f0397
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-06-27 11:03:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Fetch movies from MongoDB sorted by votes in descending order
-    movies = list(movie_collection.find().sort([('votes', -1)]))
+    movies = movie_collection.find().sort('votes', -1)
     return render_template('index.html', movies=movies)
 
 @app.route('/compare')
@@ -20,12 +19,20 @@ def compare():
 def vote():
     winner_id = request.form['winner_id']
     winner_title = request.form['winner_title']
+    non_winner_id = request.form['non_winner_id']
 
     # Update vote count for the winner in MongoDB
-    # Try to find the movie by ID, if not found, create a new document
     movie = movie_collection.find_one({'id': winner_id})
-    if movie:
-        movie_collection.update_one({'id': winner_id}, {'$inc': {'votes': 1}})
+    non_winner = movie_collection.find_one({'id': non_winner_id})
+    if non winner:
+        non_winner_votes = non_winner.get('votes')
+        add_votes = non_winner_votes//2
+        if add_votes == 0:
+            add_votes = 1
+        if movie:
+            movie_collection.update_one({'id': winner_id}, {'$inc': {'votes': add_votes}})
+        else:
+            movie_collection.insert_one({'id': winner_id, 'title': winner_title, 'votes': add_votes})
     else:
         movie_collection.insert_one({'id': winner_id, 'title': winner_title, 'votes': 1})
 
